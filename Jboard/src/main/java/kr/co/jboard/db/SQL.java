108a3aa2c3a40705dd11bede67a3b6213749104c
oink24 <oooq3o@gmail.com>
2023-08-04 16:16:17 +0900

    SQL문 수정

--- Jboard/src/main/java/kr/co/jboard/db/SQL.java
+++ Jboard/src/main/java/kr/co/jboard/db/SQL.java
@@ -3,9 +3,19 @@ package kr.co.jboard.db;
 public class SQL {
 	
 	// user
-	public static final String INSERT_USER = "INSERT INTO `user` (`uid`, `pass`, `name`, `nickname`, `email`, `hp`, `zip`, `addr1`, `addr2`, `regip`, `regDate`)"
-														+ " VALUES (?,SHA2(?, 256),?,?,?,?,?,?,?,?,NOW())";
-	public static final String SELECT_USER = "SELECT * FROM `user` WHERE `uid`=? AND `PASS`=SHA2(?, 256)";
+	public static final String INSERT_USER = "INSERT INTO `user` SET "
+											+ "`uid`=?, "
+											+ "`pass`=SHA2(?, 256), "
+											+ "`name`=?, "
+											+ "`nickname`=?, "
+											+ "`email`=?, "
+											+ "`hp`=?, "
+											+ "`zip`=?, "
+											+ "`addr1`=?, "
+											+ "`addr2`=?, "
+											+ "`regip`=?, "
+											+ "`regDate`=NOW()";
+	public static final String SELECT_USER = "SELECT * FROM `user` WHERE `uid`=? AND `pass`=SHA2(?, 256)";
 	
 	public static final String SELECT_COUNT_UID = "SELECT COUNT(*) FROM `User` WHERE `uid`=?";
 	public static final String SELECT_COUNT_NICKNAME = "SELECT COUNT(*) FROM `User` WHERE `nickname`=?";
