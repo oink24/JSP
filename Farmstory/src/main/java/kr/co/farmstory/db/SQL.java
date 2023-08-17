5b0614415b246e2253ae9871062cf03bb9025138
oink24 <oooq3o@gmail.com>
2023-08-17 17:29:05 +0900

    SQL 대소문자 수정

--- Farmstory/src/main/java/kr/co/farmstory/db/SQL.java
+++ Farmstory/src/main/java/kr/co/farmstory/db/SQL.java
@@ -3,8 +3,8 @@ package kr.co.farmstory.db;
 public class SQL {
 	
 	// user
-	public static final String SELECT_TERMS = "SELECT * FROM `terms`";
-	public static final String INSERT_USER = "INSERT INTO `user` SET "
+	public static final String SELECT_TERMS = "SELECT * FROM `Terms`";
+	public static final String INSERT_USER = "INSERT INTO `User` SET "
 											+ "`uid`=?, "
 											+ "`pass`=SHA2(?, 256), "
 											+ "`name`=?, "
@@ -16,7 +16,7 @@ public class SQL {
 											+ "`addr2`=?, "
 											+ "`regip`=?, "
 											+ "`regDate`=NOW()";
-	public static final String SELECT_USER = "SELECT * FROM `user` WHERE `uid`=? AND `pass`=SHA2(?, 256)";
+	public static final String SELECT_USER = "SELECT * FROM `User` WHERE `uid`=? AND `pass`=SHA2(?, 256)";
 	
 	public static final String SELECT_COUNT_UID = "SELECT COUNT(*) FROM `User` WHERE `uid`=?";
 	public static final String SELECT_COUNT_NICKNAME = "SELECT COUNT(*) FROM `User` WHERE `nickname`=?";
