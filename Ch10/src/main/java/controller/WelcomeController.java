63ba1a3ffb1eacf7d9684a6d783ab310392d939f
oink24 <oooq3o@gmail.com>
2023-08-24 09:56:54 +0900

    Ch10. MVC 기본 실습

---
+++ Ch10/src/main/java/controller/WelcomeController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome.do")
+public class WelcomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// View 포워딩
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/welcome.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
---
+++ Ch10/src/main/webapp/WEB-INF/hello.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>hello</title>
+		<!-- 
+			날짜 : 2023/08/24
+			이름 : 강나은
+			내용 : MVC 실습
+		 -->
+	</head>
+	<body>
+		<h1>Hello</h1>
+		
+		<a href="/Ch10">메인</a>
+		<a href="/Ch10/hello.do">hello</a>
+		<a href="/Ch10/welcome.do">welcome</a>
+		<a href="/Ch10/greeting.do">greeting</a>
+	</body>
+</html>
